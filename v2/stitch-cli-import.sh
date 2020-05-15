fd140dde2dc16a10f8449b337ceac8a531eba945
Michael Lynn <merlynn@gmail.com>
2020-05-15 12:50:01 -0400

    remove v2

--- v2/stitch-cli-import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source .env
-#stitch-cli login --api-key=$STITCH_API_PUBLIC_KEY --private-api-key=$STITCH_API_PRIVATE_KEY
-
-# change the value of --app-id=zerotolive-njchv
-stitch-cli import \
-  --app-id=$STITCH_APP_ID \
-  --include-hosting
- 
